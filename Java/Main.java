e8161f9aa3aae745fc37f5a3926e01ea80be844b
SibaK62 <125286430+SibaK62@users.noreply.github.com>
2023-07-10 13:25:19 +0200

    Add files via upload

---
+++ Java/Main.java
@@ -0,0 +1,11 @@
+//Fig. 2.1: Welcome.java
+// Text-printing program.
+
+public class Main
+{
+//main method begins execution of Java application
+public static void main(String[] args)
+{
+System.out.println("Welcome to Class 2!");
+} // end method main
+} // end class Welcome1
