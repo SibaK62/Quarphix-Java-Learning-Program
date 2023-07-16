68e44a0513555fa69783bbe9698cad8b0ca1a25b
siwaphiweGili <114389718+siwaphiweGili@users.noreply.github.com>
2023-07-16 13:02:05 +0200

    Add files via upload

---
+++ Chapter 2/Main.java
@@ -0,0 +1,13 @@
+// Fig. 2.2: Welcome.java
+// Text-printing program.
+
+public class Main
+{
+    //main method begins execution of Java application
+    public static void main (String[] args)
+    {
+        System.out.println("Welcome to Java Programming!");
+    }// end method main
+} //end class Welcome1
+
+    
---
+++ Chapter 2/Main2.java
@@ -0,0 +1,10 @@
+public class Main2 
+{
+    //main method begins execution of Java application
+    public static void main(String[] args)
+    {
+        System.out.print("Welcome to");
+        System.out.println(" Java Class2!");
+    }// end method main
+    
+}// end class Welcome2
